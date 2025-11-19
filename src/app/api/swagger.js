6173236f3eb362ee3a4f98e683144267155f6067
Oscar <oskharm12@gmail.com>
2025-11-18 21:08:45 -0300

    alterando url da api

--- src/app/api/swagger.js
+++ src/app/api/swagger.js
@@ -1,4 +1,5 @@
 import swaggerJsdoc from "swagger-jsdoc";
+const url = process.env.PUBLIC_URL
 
 const options = {
   definition: {
@@ -10,7 +11,7 @@ const options = {
     },
     servers: [
       {
-        url: "http://localhost:3000/api", // base da sua API
+        url: `${url}/api`, // base da sua API
       },
     ],
   },
