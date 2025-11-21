8bd28ed617d2242a523a8f0e7f736eee18e0b16b
Oscar <oskharm12@gmail.com>
2025-11-21 00:44:06 -0300

    Atualização da aplicação

--- script.sh
+++ script.sh
@@ -2,3 +2,7 @@ docker build -t Lunme .
 docker stop Lunme
 docker rm Lunme
 docker run -d --env-file .env -p 3000:3000 --name Lunme Lunme
+
+git add *
+git commit -m "Atualização da aplicação"
+git push origin main
